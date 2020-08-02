105ae4b6b5c011c5fa2901d991b264a282238d2f
Dean Warren <deawar@gmail.com>
2020-08-02 00:52:58 -0400

    Updated Database to CloudAtlas and .env

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require("express");
-
+require('dotenv').config();
 const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
-
+// MongoDB cloud URL
+const { MONGODB_URI } = process.env;
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -16,7 +17,19 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
+// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
+mongoose.connect(MONGODB_URI, {
+  // All these to get rid of Mongoose warnings
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+});
+mongoose.connection.on('connected', () => {
+  console.log('Sucessfully connected to MongoDb Cloud');
+});
+mongoose.connection.on('error', (error) => {
+  console.log('Database Error:', error);
+});
 
 // Start the API server
 app.listen(PORT, function() {
